f135953959d96738438cc571edb012d7d44bb463
lokesh5608 <jbnwraps22@gmail.com>
2024-10-24 19:07:17 +0530

    Add files via upload

---
+++ raam.js
@@ -0,0 +1,81 @@
+// code for work section ----1
+
+const carousel = document.querySelector(".carousel");
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
+
+const showHideIcons = () => {
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display =
+    carousel.scrollLeft == scrollWidth ? "none" : "block";
+};
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14;
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    showHideIcons();
+    setTimeout(() => showHideIcons(), 60);
+  });
+});
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positiondiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positiondiff;
+  showHideIcons();
+};
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragging);
+
+// code for menu icon
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// typing animation code
+
+const typed = new Typed(".multiple-text", {
+  strings: ['VIDEO EDITOR', 'GRAPHIC DESIGNER','INFLUENCER'],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 120,
+  loop: true,
+});
+

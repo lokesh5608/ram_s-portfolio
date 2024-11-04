6f43766afcec57f8da04ff698f95a709a0521fdc
lokesh mehta <worklokesh57@gmail.com>
2024-11-04 13:38:45 +0530

    add preloader

--- raam.js
+++ raam.js
@@ -79,3 +79,12 @@ const typed = new Typed(".multiple-text", {
   loop: true,
 });
 
+// preloader js code
+
+// window.addEventListener("load", function() {
+//   // Hide the preloader
+//   document.getElementById("preloader").style.display = "none";
+  
+//   // Show the main content
+//   document.getElementById("content").style.display = "block";
+// });
